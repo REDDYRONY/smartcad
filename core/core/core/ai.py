ae378e343d85ba855959ebe89aff0390fc86511d
DHANIKELA REDDAIAH YADAV <151990146+REDDYRONY@users.noreply.github.com>
2026-05-04 13:13:26 +0530

    /core directory

--- core/core/core/ai.py
+++
@@ -1,6 +0,0 @@
-def suggest(stress):
-    if stress > 200:
-        return "Increase thickness"
-    elif stress < 50:
-        return "Reduce material"
-    return "Design is optimal"
--- core/core/core/cad/viz/graph.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-def generate_graph():
-    t = range(1, 50)
-    stress = [1000 / (20 * x) for x in t]
-
-    plt.plot(t, stress)
-    plt.savefig("graph.png")
